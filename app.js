9f1b1f4d130b23a973bd37486b1c7397eaf0dc41
Agnes Chiang <agnesninja@gmail.com>
2017-11-30 04:32:30 +1300

    Add project folders and files

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
+ 
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
